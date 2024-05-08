13a4d5e184a7fdf50c0fe084ebe47012ccb1c04d
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2024-05-08 18:25:30 +0300

    added simple nodes with component inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useCallback } from "react";
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from "reactflow";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "reactflow/dist/style.css";
+
+import TextUpdaterNode from "./components/TextUpdaterNode";
+import initialNodes from "./nodes";
+import initialEdges from "./edges";
+
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  console.log(nodes);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant="dots" gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
 }
-
-export default App
---
+++ src/nodes.jsx
@@ -0,0 +1,28 @@
+import SimpleComponent from "./components/SimpleComponent";
+export default [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Input Node" },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: "2",
+    data: {
+      label: (
+        <div>
+          {" "}
+          <SimpleComponent />
+        </div>
+      ),
+    },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "3",
+    type: "output",
+    data: { label: "Output Node" },
+    position: { x: 250, y: 250 },
+  },
+];
